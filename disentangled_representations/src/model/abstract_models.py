3271d9f83001cc8b29be129735b068479f66c82b
yakorch <korchyar19@gmail.com>
2025-04-24 16:35:08 +0300

    add UNet I2I model. add top-level class that communicates with different components.

---
+++ disentangled_representations/src/model/abstract_models.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractmethod
+from typing import final, Optional, Sequence
+
+import torch
+import torch.nn as nn
+
+
+class I2IModel(nn.Module, ABC):
+    @abstractmethod
+    def forward(self, x: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: image
+        :param z: AdaIN parameters.
+        """
+        pass
+
+    @abstractmethod
+    def compute_structural_embedding(self, x: torch.Tensor) -> tuple[torch.Tensor, Optional[Sequence[torch.Tensor]]]:
+        """
+        :param x: image
+        :return: feature map of the lowest spatial resolution; some intermediate auxiliary tensors could be the 2nd argument, such as skip connections.
+        """
+        pass
+
+    @abstractmethod
+    def enrich_structural_embedding(self, feature_map: torch.Tensor, style_params: torch.Tensor) -> torch.Tensor:
+        """
+        :param feature_map: low-res feature map.
+        :param style_params: parameters used to enrich the feature map. for example, AdaIN parameters.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def reconstruct(self, styled_feature_map: torch.Tensor, auxiliary_tensors: Optional[Sequence[torch.Tensor]]) -> torch.Tensor:
+        pass
+
+
+class VariationalTransientEncoder(nn.Module, ABC):
+    @abstractmethod
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: image.
+        :return: z_params -- variational (unsampled) latent vector parameters.
+        """
+        pass
+
+    @staticmethod
+    @final
+    def sample_from_multivariate_normal(z_params: torch.Tensor) -> torch.Tensor:
+        mu, log_variance = z_params.chunk(2, dim=1)
+        std = torch.exp(0.5 * log_variance)
+        eps = torch.randn_like(std)
+        z = mu + eps * std
+        return z
---
+++ disentangled_representations/src/model/model_kapellmeister.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from typing import Sequence, Optional
+
+import torch
+import torch.nn as nn
+
+from .abstract_models import I2IModel, VariationalTransientEncoder
+
+
+@dataclass
+class HiddenParams:
+    transient_params: torch.Tensor
+    transient_sample: Optional[torch.Tensor]
+    structural_feature_map: torch.Tensor
+
+
+@dataclass
+class ReconstructionMetadata:
+    hidden_params: HiddenParams
+
+    style_params: torch.Tensor
+    auxiliary: Sequence[torch.Tensor]
+    reconstruction: torch.Tensor
+
+
+@dataclass
+class AllReconstructionsIntermediateMetadata:
+    a_recon_metadata: ReconstructionMetadata
+    b_recon_metadata: ReconstructionMetadata
+
+    a_hat_hidden_params: HiddenParams
+    b_hat_hidden_params: HiddenParams
+
+
+class Kapellmeister:
+    def __init__(self, I2I_model: I2IModel, variational_transient_encoder: VariationalTransientEncoder, style_params_MLP: nn.Module) -> None:
+        self.I2I_model = I2I_model
+
+        self.variational_transient_encoder = variational_transient_encoder
+        self.style_params_MLP = style_params_MLP
+
+    def self_reconstruction(self, x: torch.Tensor) -> ReconstructionMetadata:
+        """
+        :param x: image.
+        """
+        z_params = self.variational_transient_encoder(x)
+        z = VariationalTransientEncoder.sample_from_multivariate_normal(z_params)
+        style_params = self.style_params_MLP(z)
+
+        x_structure, x_auxiliary = self.I2I_model.compute_structural_embedding(x)
+        x_structure_enriched = self.I2I_model.enrich_structural_embedding(x_structure, style_params)
+        x_hat = self.I2I_model.reconstruct(x_structure_enriched, x_auxiliary)
+
+        return ReconstructionMetadata(hidden_params=HiddenParams(transient_params=z_params, transient_sample=z, structural_feature_map=x_structure),
+                                      style_params=style_params, auxiliary=x_auxiliary, reconstruction=x_hat)
+
+    def all_reconstructions(self, a: torch.Tensor, b: torch.Tensor) -> AllReconstructionsIntermediateMetadata:
+        a_recon_metadata = self.self_reconstruction(a)
+        b_recon_metadata = self.self_reconstruction(b)
+
+        a_structure_enriched_with_b_style = self.I2I_model.enrich_structural_embedding(a_recon_metadata.hidden_params.structural_feature_map,
+                                                                                       b_recon_metadata.style_params)
+        b_hat = self.I2I_model.reconstruct(a_structure_enriched_with_b_style, a_recon_metadata.auxiliary)
+
+        b_structure_enriched_with_a_style = self.I2I_model.enrich_structural_embedding(b_recon_metadata.hidden_params.structural_feature_map,
+                                                                                       a_recon_metadata.style_params)
+        a_hat = self.I2I_model.reconstruct(b_structure_enriched_with_a_style, b_recon_metadata.auxiliary)
+
+        a_hat_structure, _ = self.I2I_model.compute_structural_embedding(a_hat)
+        b_hat_structure, _ = self.I2I_model.compute_structural_embedding(b_hat)
+
+        a_hat_latent_params = self.variational_transient_encoder(a_hat)
+        b_hat_latent_params = self.variational_transient_encoder(b_hat)
+
+        return AllReconstructionsIntermediateMetadata(a_recon_metadata=a_recon_metadata, b_recon_metadata=b_recon_metadata,
+                                                      a_hat_hidden_params=HiddenParams(transient_params=a_hat_latent_params, transient_sample=None,
+                                                                                       structural_feature_map=a_hat_structure),
+                                                      b_hat_hidden_params=HiddenParams(transient_params=b_hat_latent_params, transient_sample=None,
+                                                                                       structural_feature_map=b_hat_structure))
