806a15ef30c8afe11e1eef7755924650fb747270
yakorch <korchyar19@gmail.com>
2025-04-26 10:48:19 +0300

    add symlinks script to `all.sh`

--- scripts/dataset_restructure/all.sh
+++ scripts/dataset_restructure/all.sh
@@ -1,3 +1,4 @@
+bash scripts/dataset_restructure/create_symlinks.sh
 bash scripts/dataset_restructure/BANDON_1.sh
 bash scripts/dataset_restructure/BANDON_2.sh
 bash scripts/dataset_restructure/GVLM_CD.sh
