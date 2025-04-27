cbf88f842efa24bdaae106c719134390a60e9546
yakorch <korchyar19@gmail.com>
2025-04-27 15:37:22 +0300

    try only 1 dataset training.

--- disentangled_representations/src/data_processing/aerial_dataset_loaders.py
+++ disentangled_representations/src/data_processing/aerial_dataset_loaders.py
@@ -7,8 +7,11 @@ from .aerial_dataset_instances import (BANDON_dataset_train, BANDON_dataset_val,
 
 
 def create_train_data_loader_for_image_pairs(batch_size: int, num_workers: int):
-    dataset_weights_mapping_train: dict[torch.utils.data.Dataset, float] = {LEVIR_dataset_train: 1.5, SYSU_dataset_train: 0.1, S2Looking_dataset_train: 0.25,
-                                                                            Hi_UCD_dataset_train: 0.5, GVLM_dataset: 25.0, BANDON_dataset_train: 4.0, }
+    # dataset_weights_mapping_train: dict[torch.utils.data.Dataset, float] = {LEVIR_dataset_train: 1.5, SYSU_dataset_train: 0.1, S2Looking_dataset_train: 0.25,
+    #                                                                         Hi_UCD_dataset_train: 0.5, GVLM_dataset: 25.0, BANDON_dataset_train: 4.0, }
+
+    dataset_weights_mapping_train: dict[torch.utils.data.Dataset, float] = {Hi_UCD_dataset_train: 1.0}
+
     train_datasets = list(dataset_weights_mapping_train.keys())
 
     logger.info(f"{({ds.__class__.__name__: len(ds) for ds in train_datasets})=}")
@@ -32,8 +35,11 @@ def create_train_data_loader_for_image_pairs(batch_size: int, num_workers: int):
 
 
 def create_val_data_loader_for_image_pairs(batch_size: int, num_workers: int):
-    dataset_weights_mapping_val: dict[torch.utils.data.Dataset, float] = {SYSU_dataset_val: 0.02, S2Looking_dataset_val: 0.1, Hi_UCD_dataset_val: 0.15,
-                                                                          BANDON_dataset_val: 0.5}
+    # dataset_weights_mapping_val: dict[torch.utils.data.Dataset, float] = {SYSU_dataset_val: 0.02, S2Looking_dataset_val: 0.1, Hi_UCD_dataset_val: 0.15,
+    #                                                                       BANDON_dataset_val: 0.5}
+
+    dataset_weights_mapping_val: dict[torch.utils.data.Dataset, float] = {Hi_UCD_dataset_train: 0.25}
+
     val_datasets = list(dataset_weights_mapping_val.keys())
 
     logger.info(f"{({ds.__class__.__name__: len(ds) for ds in val_datasets})=}")
